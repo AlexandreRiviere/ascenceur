508adec9fd7d37c010c59c83be37328bfdc71f45
ThomasEscam <thomas.escamilla.81@gmail.com>
2020-11-21 16:41:46 +0100

    Ajout des méthodes pour les BP de l'étage 4

--- programmeAscenseur/programmeAscenseur.ino
+++ programmeAscenseur/programmeAscenseur.ino
@@ -1,14 +1,21 @@
-
+int ledMotM = 14;
 int ledMotD = 15;
 int ledEt1 = 45;
 int ledDetecEt1 =13;
 int ledEt2 = 44;
 int ledEt3 = 43;
 int ledEt4 = 42;
+int ledEt5 = 41;
 int ledDetecEt2 =12;
 int ledDetecEt3 =11;
 int ledDetecEt4 =10;
 int ledDetecEt5 =9;
+int ledUrgence = 46;
+
+int ledEt4M = 26;
+int ledEt4D = 24;
+int boutonEt4M = 34;
+int boutonEt4D = 31;
 
 void setup() {
   pinMode(ledUrgence, OUTPUT);
@@ -19,7 +26,11 @@ void setup() {
   pinMode(ledEt1,INPUT);
   pinMode(ledEt2,INPUT);
   pinMode(ledEt3,INPUT);
-  pinMode(ledEt4,INPUT);
+  pinMode(ledEt4,OUTPUT);
+  pinMode(ledEt4M,OUTPUT);
+  pinMode(ledEt4D,OUTPUT);
+  pinMode(boutonEt4M,INPUT);
+  pinMode(boutonEt4D,INPUT);
   pinMode(ledDetecEt1,OUTPUT);
   pinMode(ledDetecEt2,OUTPUT);
   pinMode(ledDetecEt3,OUTPUT);
@@ -94,7 +105,41 @@ void allerEtage4() {
         digitalWrite(ledMotM,LOW);
     }
 }
+void monterDepuisEtage4(){
+  if(digitalRead(ledDetecEt1)== HIGH || digitalRead(ledDetecEt2)== HIGH || digitalRead(ledDetecEt3)== HIGH){
+    if (digitalRead(ledEt4M) == HIGH){
+        digitalWrite(ledEt4M,HIGH);
+        digitalWrite(ledMotM,HIGH); 
+    }
+  }else {
+    if(digitalRead(ledEt4M) == HIGH){
+        digitalWrite(ledEt4M,HIGH);
+        digitalWrite(ledMotD,HIGH); 
+    }
+  }
+  if(digitalRead(ledDetecEt4) == HIGH){
+        digitalWrite(ledMotD,LOW);
+        digitalWrite(ledMotM,LOW);
+  }
+}
 
+void descendreDepuisEtage4() {
+  if(digitalRead(ledDetecEt1)== HIGH || digitalRead(ledDetecEt2)== HIGH || digitalRead(ledDetecEt3)== HIGH){
+    if(digitalRead(ledEt4D) == HIGH){
+        digitalWrite(ledEt4D,HIGH);
+        digitalWrite(ledMotM,HIGH); 
+    }
+  }else {
+    if (digitalRead(ledEt4D) == HIGH){
+        digitalWrite(ledEt4D,HIGH);
+        digitalWrite(ledMotD,HIGH); 
+    }
+  }
+  if(digitalRead(ledDetecEt4) == HIGH){
+        digitalWrite(ledMotD,LOW);
+        digitalWrite(ledMotM,LOW);
+  }
+}
 void allerEtage5() {
     if(digitalRead(ledEt5) == HIGH){
         digitalWrite(ledEt5,HIGH);
